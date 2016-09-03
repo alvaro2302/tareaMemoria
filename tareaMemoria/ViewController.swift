4cc97e6182fa44eea0ec349c59463254bc1f5a4c
Alex Cuiza <alexcuiza@alex-cuizas-macbook.local>
2016-09-03 18:17:44 -0400

    cambio

--- tareaMemoria/ViewController.swift
+++ tareaMemoria/ViewController.swift
@@ -12,12 +12,44 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        buscarNumero()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+ 
+    //aqui esta la tarea no puedo subirlo con playgroud
+    func buscarNumero(){
+        
+        
+        for numero in 0...100{
+            
+            if numero % 5 == 0{
+                
+                print("\(numero) Bingo!!")
+                
+            }
+            if numero % 2 == 0{
+                
+                print("\(numero)  par")
+                
+            }
+            else{
+                
+                print("\(numero) impar")
+            }
+            if numero >= 30 && numero <= 40 {
+                
+                print("\(numero) viva swift!!")
+                
+            }
+            
+        }
+
+        
     }
 
 
